6bc3526dd94637ef3b59210a8b687875716bfb89
Isra GG <wuachimingo271@gmail.com>
2021-10-19 13:55:08 -0500

    Agregar archivos de proyecto.

---
+++ PruebaArreglo/Alumno.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaArreglo
+{
+    class Alumno
+    {
+        private string nombre;
+        private string matricula;
+        private string carrera;
+
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+
+
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public Alumno() 
+        {
+            nombre = "";
+            matricula = "";
+            carrera = "";
+        }
+       public Alumno(string nombre, string matricula, string carrera) 
+        {
+            this.nombre = nombre;
+            this.matricula = matricula;
+            this.carrera = carrera;
+       }
+        public override string ToString()
+        {
+            return nombre + "" + matricula + "" + carrera;
+        }
+    } 
+
+    
+    
+}
---
+++ PruebaArreglo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PruebaArreglo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno[] al = new Alumno[5];
+            for (int i = 0; i <al.Length; i++)
+            {
+                al[i] = new Alumno();
+                
+                al[i].Nombre = "Nombre: Pepito";
+                al[i].Matricula = " Matricula: I219482";
+                al[i].Carrera = " Carrera: Informatica";
+            }
+            for (int i = 0; i <al. Length; i++)
+            {
+                Console.WriteLine(al[i].ToString());
+            //Profe no supe donde agregar el WriteLine y el ReadLine, una disculpa.
+            }
+           
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
