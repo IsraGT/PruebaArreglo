17ae1208bb0947ff928900f309bfcaf93ad42e7f
Isra GG <wuachimingo271@gmail.com>
2021-10-21 11:24:21 -0500

    se agrego Console.ReadLine

--- PruebaArreglo/Program.cs
+++ PruebaArreglo/Program.cs
@@ -10,15 +10,20 @@ namespace PruebaArreglo
             for (int i = 0; i <al.Length; i++)
             {
                 al[i] = new Alumno();
+
                 
-                al[i].Nombre = "Nombre: Pepito";
-                al[i].Matricula = " Matricula: I219482";
-                al[i].Carrera = " Carrera: Informatica";
+                Console.WriteLine("Nombre:");
+                al[i].Nombre = Console.ReadLine();
+                Console.WriteLine(" Matricula:");
+                al[i].Matricula = Console.ReadLine();
+                Console.WriteLine(" Carrera:");
+                al[i].Carrera = Console.ReadLine();
+
             }
             for (int i = 0; i <al. Length; i++)
             {
                 Console.WriteLine(al[i].ToString());
-            //Profe no supe donde agregar el WriteLine y el ReadLine, una disculpa.
+           
             }
            
 
